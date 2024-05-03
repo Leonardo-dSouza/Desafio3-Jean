c11f1f3d8e51911b79db2cb09706c7e62c4d2b7a
Leonardo-dSouza <dkaireturn@gmail.com>
2024-05-03 05:18:53 -0300

    Finalizando v3

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template
 app = Flask("__name__")
 
-@app.route("/")
+@app.route("/index")
 def index():
     return render_template("index.html")
+
+@app.route("/caska")
+def SobreCaska():
+    return render_template("caska.html")
+
+
+@app.route("/guts")
+def SobreGuts():
+    return render_template("guts.html")
+
+@app.route("/griffth")
+def SobreGriffth():
+    return render_template("griffth.html")
